e9f83531873567b866cf37b1738c8bb95bcd7233
TeodoraObucina <teodoraobucina22@gmail.com>
2024-10-22 00:11:57 +0200

    dodati modeli za kurs i video i migracije

---
+++ Laravel/app/Models/Course.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Course extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'description', 'teacher_id'];
+
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
+}
